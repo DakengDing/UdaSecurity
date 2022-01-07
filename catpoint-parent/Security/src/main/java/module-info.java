c02ce34436494edc5f0b16bc6b54e3abd6003c91
YUL155 <liusnakr2012@live.com>
2022-01-07 03:20:28 -0500

    step2

---
+++ catpoint-parent/Security/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module Security {
+    requires miglayout;
+    requires java.desktop;
+    requires com.google.gson;
+    requires java.prefs;
+    requires com.google.common;
+
+    opens com.udacity.catpoint.security.data to com.google.gson;
+}
